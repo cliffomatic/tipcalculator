8e41159d160027388916b6e2e25f100a21319659
Cliff Curry <ccurry@google.com>
2014-06-16 23:05:46 -0700

    Tip Calculator Homework
    @thecodepath looking forward to tomorrow.

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Cliff Curry on 6/16/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
