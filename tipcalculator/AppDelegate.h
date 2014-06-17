aa26118ef1c317129d25c86166d668408872c659
Cliff Curry <ccurry@google.com>
2014-06-16 19:44:23 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Cliff Curry on 6/16/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
